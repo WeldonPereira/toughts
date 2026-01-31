9fb79e5228a4f5e700eb4e26d12f25cf775f9d78
Weldon <weldonbarros179@gmail.com>
2026-01-31 14:30:00 -0300

    feat: initialize express server and basic middleware

---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const { engine } = require("express-handlebars");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
+const flash = require("express-flash");
+
+const app = express();
+
+const conn = require("./db/conn");
+
+const Tought = require("./models/Toughts");
+const User = require("./models/User");
+
+const toughtsRoutes = require("./routes/toughtsRoutes");
+const ToughtController = require("./controllers/ToughtController");
+
+const authRoutes = require("./routes/authRoutes");
+const AuthController = require("./controllers/AuthController");
+
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
+
+app.use(express.json());
+
+app.use(
+  session({
+    name: "session",
+    secret: "nosso_secret",
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+      logFn: function () {},
+      path: require("path").join(require("os").tmpdir(), "sessions"),
+    }),
+    cookie: {
+      secure: false,
+      maxAge: 360000,
+      expires: new Date(Date.now() + 360000),
+      httpOnly: true,
+    },
+  }),
+);
+
+app.use(flash());
+
+app.use(express.static("public"));
+
+app.use((req, res, next) => {
+  if (req.session.userid) {
+    res.locals.session = req.session;
+  }
+  next();
+});
+
+app.use("/toughts", toughtsRoutes);
+app.get("/", ToughtController.showToughts);
+app.get("/dashboard", ToughtController.dashboard);
+
+app.use("/", authRoutes);
+app.get("/login", AuthController.login);
+app.get("/register", AuthController.register);
+
+conn
+  .sync()
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
